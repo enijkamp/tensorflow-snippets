e1567745e7a47277147c76b954db1978548611c5
Erik Nijkamp <erik.nijkamp@gmail.com>
2018-04-15 16:10:05 -0700

    ...

--- shape.py
+++ shape.py
@@ -5,8 +5,8 @@ with tf.Session() as sess:
     x = tf.placeholder(tf.int32, shape=[4])
     print(x.get_shape()) # (4,)
 
-    y, _ = tf.unique(x) # (?,)
-    print(y.get_shape())
+    y, _ = tf.unique(x)
+    print(y.get_shape()) # (?,)
     print(y.eval(feed_dict={x: [0, 1, 2, 3]}).shape) # (4,)
     print(y.eval(feed_dict={x: [0, 0, 0, 0]}).shape) # (1,)
 
